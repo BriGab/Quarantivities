411000ff1d9c84c5c724ca3ea88901a8b67c4bbf
Bri Gab <brigabiola@outlook.com>
2020-04-03 15:13:44 -0600

    user model testing

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,14 +3,31 @@ import { Link } from "react-router-dom";
 
 
 function SignUp () {
+    
+    const [signUp, setSignUp] = useState({
+        userName: "",
+        password: ""
+    })
 
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        console.log(event.target.value)
+        setSignUp({...signUp, [name]: value})
+        console.log(signUp);
+    }
 
-
-
+    function handleFormSubmit (event) {
+        event.preventDefault();
+        if(signUp.userName && signUp.password) {
+            
+        }
+    }
 
     return (
         <div>
-            
+            <input type="text" placeholder= "userName Here" onChange={handleInputChange} name="userName"></input>
+            <input type="text" placeholder= "Password Here" onChange={handleInputChange} name="password"></input>
+            <button type="submit" onClick={handleFormSubmit}>Button</button>
         </div>
     )
 }
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     // here we will require the name of the model(s) we create for example book.js holding the book model would be book: require("./book")
-    model: require("./model")
+    User: require("./user")
 }
