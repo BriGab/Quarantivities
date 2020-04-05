8087b3a62bf9a6aa033560d2637d9a261bdd92fa
LMField82 <meneslindsay@hotmail.com>
2020-04-05 11:38:42 -0600

    updated models files adds appointment file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     // here we will require the name of the model(s) we create for example book.js holding the book model would be book: require("./book")
     User: require("./user"),
-    Activity: require("./Activity")
+    Activity: require("./Activity"),
+    Appointment: require("./Appointment")
     
 }
