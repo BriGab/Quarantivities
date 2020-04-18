0fb96dc7374a70b3c1d3212d8be5099c7e628716
Caitlin Trask <caitlintrask@gmail.com>
2020-04-18 08:58:45 -0600

    added explore card on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,12 @@ import API from "../utils/API";
 import Nav from "../components/Nav";
 // import { Link, useParams } from "react-router-dom";
 import "../styles/Home.css"
-import { Container, Card, Button, Row, Col, ListGroup, alertClicked, Modal, handleshow} from "react-bootstrap";
+import { Container, Card, Button, Row, Col, ListGroup, alertClicked, Modal, handleshow } from "react-bootstrap";
 import { UserActivity, UserTitle } from "../components/UserActivity";
 import { CardList } from "../components/CardList"
 import { CardListItem } from "../components/CardList";
 
-function Home () {
+function Home() {
 
     const [popularactivity, setActivities] = useState([]);
     const [userCreatedActivity, setUserCreatedActivity] = useState([]);
@@ -16,37 +16,37 @@ function Home () {
     useEffect(() => {
         loadPopular();
         loadAct();
-      }, []);
+    }, []);
 
     function loadPopular() {
         API.fetchPopular()
-        .then(popularactivity => {
-          setActivities(popularactivity.data);
-        })
-        .catch(err => console.log(err))
-    }
-    
-    function loadAct () {
-        API.getActivity()
-        .then(res=> {
-            let userAct = res.data[0].activity
-            console.log("user activity", userAct)
-            setUserCreatedActivity(userAct)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then(popularactivity => {
+                setActivities(popularactivity.data);
+            })
+            .catch(err => console.log(err))
     }
 
-    return ( 
-    <div id="homeID">
-        <Nav />
-        <Container>
-            <Row>
-                <Col>
-                    <Card className="favorites-card">
-                        <Card.Body>
-                            <Card.Title className="card-title">TOP 5 MOST LIKED QUARANTIVITIES</Card.Title>
+    function loadAct() {
+        API.getActivity()
+            .then(res => {
+                let userAct = res.data[0].activity
+                console.log("user activity", userAct)
+                setUserCreatedActivity(userAct)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    return (
+        <div id="homeID">
+            <Nav />
+            <Container>
+                <Row>
+                    <Col>
+                        <Card className="favorites-card">
+                            <Card.Body>
+                                <Card.Title className="card-title">TOP 5 MOST LIKED QUARANTIVITIES</Card.Title>
                                 <CardList className="topFive">
                                     {popularactivity.map(popularactivity => {
                                         return (
@@ -59,47 +59,68 @@ function Home () {
                                                 likes={popularactivity.likes}
                                                 category={popularactivity.category}
                                             />
-                                            )
-                                        })}
+                                        )
+                                    })}
                                 </CardList>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                            </Card.Body>
+                        </Card>
+                    </Col>
 
-             <Col>
-                 <Card className="planner-card">
-                    <Card.Body>
-                        <Card.Title>ADD YOUR OWN ACTIVITY</Card.Title>
-                        <Card.Text>
-                                Found an activity you love and want to do again? Add it here! 
+                    <Col>
+                        <Card className="planner-card">
+                            <Card.Body>
+                                <Card.Title>ADD YOUR OWN ACTIVITY</Card.Title>
+                                <Card.Text>
+                                    Found an activity you love and want to do again? Add it here!
                         </Card.Text>
-                        <UserActivity />
-                        <ListGroup> 
-                        {/* <ListGroup defaultActiveKey="#link1" variant="flush,primary">  */}
-                        <div>
-                            {!userCreatedActivity.length ? (
-                                <h2 className="text-center">You havent Added Any Activities</h2>
-                                ) : (
-                                 <ListGroup.Item>
-                                    {userCreatedActivity.map(userAct => {
-                                        return (
-                                            <UserTitle 
-                                            key={userAct._id}
-                                            title={userAct.title} 
-                                            href={userAct.href}
-                                            />
-                                            )
-                                })}
-                                </ListGroup.Item>
-                            )}
-                        </div>
-                        </ListGroup>
-                    </Card.Body>
-                </Card>
-            </Col>
-            </Row>
-        </Container>
-    </div>
+                                <UserActivity />
+                                <ListGroup>
+                                    {/* <ListGroup defaultActiveKey="#link1" variant="flush,primary">  */}
+                                    <div>
+                                        {!userCreatedActivity.length ? (
+                                            <h2 className="text-center">You havent Added Any Activities</h2>
+                                        ) : (
+                                                <ListGroup.Item>
+                                                    {userCreatedActivity.map(userAct => {
+                                                        return (
+                                                            <UserTitle
+                                                                                                                                                                                                        key={userAct._id}
+                                                                title={userAct.title}
+                                                                href={userAct.href}
+                                                            />
+                                                        )
+                                                    })}
+                                                </ListGroup.Item>
+                                            )}
+                                    </div>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Card className="explore-card">
+                <Card.Body>
+                    <Card.Title className="card-title">EXPLORE QUARANTIVITIES</Card.Title>
+                    <CardList className="topFive">
+                        {popularactivity.map(popularactivity => {
+                            return (
+                                <CardListItem
+                                    key={popularactivity._id}
+                                    id={popularactivity._id}
+                                    title={popularactivity.title}
+                                    href={popularactivity.href}
+                                    description={popularactivity.description}
+                                    likes={popularactivity.likes}
+                                    category={popularactivity.category}
+                                />
+                            )
+                        })}
+                    </CardList>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
