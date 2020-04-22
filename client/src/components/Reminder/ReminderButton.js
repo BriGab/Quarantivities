ba94ca4c911569e97b6f32e1d3fdf5f9090a7ada
Bri Gab <brigabiola@outlook.com>
2020-04-22 14:42:24 -0600

    playing with cards/buttons

--- client/src/components/Reminder/ReminderButton.js
+++ client/src/components/Reminder/ReminderButton.js
@@ -7,7 +7,7 @@ function ReminderButton(props) {
     return ( <>
           
               <button  className="btn reminder btn-form btn-outline-secondary btn-sm" onClick={props.addReminder}>Remind Me!</button>
-              <button className="btn-sm smallReminder"onClick={props.addReminder}><i class="fa fa-envelope-o"></i></button>
+              <button className="btn-sm btn smallReminder"onClick={props.addReminder}><i class="fa fa-envelope-o"></i></button>
   </>  )}
 
 export default ReminderButton;
