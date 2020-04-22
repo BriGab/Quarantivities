b0ff1a8dd91ab3e5d77638a9c03cbdc8ea2afa2e
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-04-21 21:15:46 -0600

    committing

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -56,30 +56,33 @@ function SignIn() {
 
 
     return (
-        <div id="signinID">
+        <div>
             <BasicNav/>
             <div>
                 {global.user && <Redirect to="" />}
 
-                <div className="container h-100 justify-content-center align-items-center center-div">
+                <div className="container h-100 justify-content-center align-items-center center-div" id="signininput">
 
                     <h4>Log in to start your fun!</h4>
 
-                    <div className="form-group" />
+                    <div className="form-group">
                     <label htmlFor="exampleInputPassword1">Email</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" onChange={handleInputChange} name="email" />
+                    </div>
 
-                    <div className="form-group" />
+                    <div className="form-group">
                     <label htmlFor="exampleInputPassword1">Password</label>
                     <input type="password" onKeyUp={handleKeyup} className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={handleInputChange} name="password" />
                     {signUp.error ? (<div style={{ fontsize: 12, color: "red" }}>{signUp.error}</div>) : null}
+                    </div>
 
-
-                    <div className="form-group form-check" />
+                    <div className="form-group form-check" id="btndiv">
                     <button type="submit" className="btn btn-primary" id="subBtn" onClick={handleFormSubmit}>Log In</button><Link to="/signin/home"></Link>
+                    </div>
+                    
+                    <br/>
 
-                    <br></br>
-                    <small>Don't have an account? Sign up <a href="/">here</a>.</small>
+                    <small id="switchBtn">Don't have an account? Sign up <a href="/">here</a>.</small>
                 </div>
             </div>
         </div>
